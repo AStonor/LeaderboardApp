c991dc98c5d9896ff761826b601f6d9761b32222
AStonor <andreas@stonor.dk>
2023-04-19 01:54:26 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,88 @@
+
+const podio = {
+
+    el: document.getElementById("topthree"),
+
+    init: function (employeesSorted) {
+        [this.el.childNodes[3], this.el.childNodes[1], this.el.childNodes[5]].forEach((el, index) => {
+            el.childNodes[3].innerText = employeesSorted[index].total;
+            el.childNodes[5].innerText = employeesSorted[index].name;
+        })
+
+    }
+}
+
+const table = {
+
+    el: document.getElementById("table"),
+
+    sortedBy: "total",
+
+    sortedFromMaxToMin: true,
+
+    init: async function () {
+        this.employees = await this.fetchData();
+        this.calcAndAddTotalToEmployees();
+        this.update();
+        podio.init(this.employees);
+    },
+
+    update: function () {
+        this.sortEmployees();
+        this.el.innerHTML = this.template();
+    },
+
+    catagories: function () {
+        return Object.keys(this.employees[0]);
+    },
+
+    sortEmployees: function () {
+        this.employees.sort((a, b) => {
+            if (a[this.sortedBy] == b[this.sortedBy]) {
+                return 0
+            } else if (this.sortedFromMaxToMin && a[this.sortedBy] > b[this.sortedBy] || !this.sortedFromMaxToMin && a[this.sortedBy] < b[this.sortedBy]) {
+                return -1;
+            } else {
+                return 1;
+            }
+        })
+    },
+
+    onClick: function (clickedCatagory) {
+        this.sortedBy === clickedCatagory ? this.sortedFromMaxToMin = !this.sortedFromMaxToMin : this.sortedBy = clickedCatagory;
+        this.update();
+    },
+
+    calcAndAddTotalToEmployees: function () {
+        this.employees.forEach(employee => {
+            employee.total = employee.academy + employee.challange;
+        })
+    },
+
+    fetchData: async function () {
+        const data = await fetch("../json/employees.json");
+        return data.json();
+    },
+
+    template: function () {
+        const catagories = this.catagories();
+        let template = `<thead><tr><th scope="col">#</th>`
+        catagories.forEach(catagory => {
+            template += `<th scope="col" style="cursor:pointer;" onclick="table.onClick('${catagory}');return false;">${catagory}</th>`
+        });
+        template += `</tr></thead><tbody>`;
+        this.employees.forEach((employee, index) => {
+            template += `<tr><th scope="row">${index + 1}</th>`
+            catagories.forEach(catagory => {
+                template += `<td>${employee[catagory] === null ? "" : employee[catagory]}</td>`
+            });
+        })
+        template += `</tr>`
+
+        return template;
+
+    }
+}
+
+table.init();
+
